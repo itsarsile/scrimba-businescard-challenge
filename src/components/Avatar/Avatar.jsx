e68277be95a1f0fd014a5e8c41739053037a1a25
itsarsile <ajiprio@hotmail.com>
2022-08-24 06:49:48 +0700

    2 files modified

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Avatar.css';
 export default function Avatar() {
-  return(
-    <div>
-      <img src="https://via.placeholder.com/317/09f/fff.png" alt="user-avatar"/>
-      </div>
-  ) 
-    
+  return (
+    <div className="avatar--wrapper">
+      <img
+        src="https://via.placeholder.com/317/09f/fff.png"
+        alt="user-avatar"
+      />
+    </div>
+  );
 }
