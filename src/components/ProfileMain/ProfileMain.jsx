9818a249f6a4b5b17ecd6b809b51b7b72d20956a
itsarsile <ajiprio@hotmail.com>
2022-08-24 07:40:06 +0700

    2 files modified

--- src/components/ProfileMain/ProfileMain.jsx
+++ src/components/ProfileMain/ProfileMain.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './ProfileMain.css';
 import About from '../ProfileInfo/About';
 import Interest from '../ProfileInfo/Interest';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillMail } from 'react-icons/ai';
 export default function SocialMedia() {
   return (
-    <div>
-      <button>Email</button>
-      <button>LinkedIn</button>
+    <div className="profilemain--wrapper">
+      <div className="profilemain--btn_wrapper">
+        <button>
+          <AiFillMail className="profilemain--btn_icon" /> Email
+        </button>
+        <button>
+          <AiFillLinkedin className="profilemain--btn_icon" />
+          LinkedIn
+        </button>
+      </div>
       <About />
       <Interest />
     </div>
